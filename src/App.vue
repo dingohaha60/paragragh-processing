290b1979347b32bbf35d469887e7102f2ab5a27d
dingohaha.1 <dingohaha.1@gmail.com>
2023-04-14 15:15:41 +0800

    first working

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+    <div>
+
+        <table style="width: 80%">
+            <td style="width: 70%">
+                <textarea v-model="thePara" style="width: 97%"></textarea>
+                <br>
+                <button @click="transfrom">转换</button>
+                <br>
+                <textarea v-model="theResult.str" style="width: 97%"></textarea>
+            </td>
+            <td style="width: 20%;vertical-align: top">
+                <textarea v-model="theEnds" style="height: 200px"></textarea>
+            </td>
+        </table>
+
+
+    </div>
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+import {reactive,ref} from "vue";
+
+// let aa="";
+let thePara = ref("")
+let theEnds = ref("")
+let theResult = reactive({})
+
+
+function transfrom() {
+    // console.log("thePara",thePara);
+    let splitted = thePara.value.split("\n");
+    let theEndsSplits = theEnds.value.split("\n");
+    let jointedThePara = splitted.join("");
+
+    // jointedThePara.split();
+    let thePattern="";
+    for (const endSplit of theEndsSplits) {
+        thePattern+="("+endSplit+")|";
+    }
+    thePattern = thePattern.substring(0,thePattern.length-1);
+    let matches = jointedThePara.matchAll(thePattern);
+    let preMatch = [];
+    preMatch["index"]=0;
+    preMatch.push("");
+    let theResultStr ="";
+    for (const match of matches) {
+        // console.log("the result splits",thePattern.substring(match.index))
+        theResultStr = theResultStr+jointedThePara.substring(preMatch.index+preMatch[0].length,match.index+match[0].length)+"\n\n";
+        // console.log("the result splits:",theResultStr);
+        preMatch = match;
+    }
+
+    theResult.str= theResultStr;
 }
+
+// import HelloWorld from './components/HelloWorld.vue'
+//
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    /*text-align: center;*/
+    color: #2c3e50;
+    margin-top: 20px;
+}
+
+textarea {
+    width: 80%;
+    height: 400px;
+    resize: vertical;
+    border: #2c3e50 2px solid;
+    font-size: 17px;
+}
+
+td {
+    border: #42b983 1px solid;
+}
+
+button {
+    width: 120px;
+    height: 40px;
 }
 </style>
